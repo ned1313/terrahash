12b6e778dfe7a6ca8fb2450f6d41fdfb645ccc98
ned1313 <ned.bellavance@gmail.com>
2024-06-10 14:48:43 -0400

    add version command and update license header

--- cmd/check.go
+++ cmd/check.go
@@ -1,5 +1,12 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 the terrahash authors
+
+Licensed under the MIT license (the "License"); you may not
+use this file except in compliance with the License.
+
+You may obtain a copy of the License at the LICENSE file in
+the root directory of this source tree.
+
 */
 package cmd
 
